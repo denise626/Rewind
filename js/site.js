37d86207a6863e4968af2517000a4d5c7baf0101
April Kiser <denise626@hotmail.com>
2021-09-18 22:08:12 -0400

    JS functions
    Adding JavaScript functions to site.js.  Adding rewind repository to git repo on project page

--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-//Call Hello World
-function HelloWorld() {
-    alert("hello World");
+//Get the string from the page
+//controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+}
+
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //last position in an array is name.length - 1
+
+
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+//revString will add to itself on every iteration of this loop
+    return revString;
+}
+
+
+//Display the message with the reversed string to the user
+//view function
+
+function displayString(revString){
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
